900cc354df9920dc2015072c1e0005d7c84bc500
Thecnfor <w5555wdnmd@gmail.com>
2026-01-21 09:53:00 +0000

    Xra优化
    将菜单组件从 index.ts 迁移到 index.tsx 并删除冗余文件
    重构头部组件文件结构，合并 header.tsx 到 index.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import localFont from "next/font/local";
 import "./globals.css";
-import { SiteHeader } from "@/components/layout/header/header";
+import { SiteHeader } from "@/components/layout/header";
 import SiteMenu from "@/components/layout/menu";
 import { AppStoreProvider } from "@/stores";
 
--- components/layout/menu/index.ts
+++
@@ -1,5 +0,0 @@
-export { default } from "./menu";
-
-export { default as MenuPanel } from "./menu-panel";
-export type { MenuPanelProps } from "./menu-panel";
-export * from "./menu-items";
